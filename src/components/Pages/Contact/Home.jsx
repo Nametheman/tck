b622eaa260ef2b14e08a824f1c1bfe0a95c9af4c
Nametheman <owoemma59@gmail.com>
2023-02-03 14:12:28 +0100

    update

--- src/components/Pages/Contact/Home.jsx
+++ src/components/Pages/Contact/Home.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import bg from "../../../assets/images/img-bg1.jpg";
+import contactUs from "../../../assets/images/contact.svg";
 
 const Home = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   return (
     <Container>
-      <div></div>
-      <div></div>
+      <FormContainer>
+        <form action="">
+          <div>
+            <label htmlFor="username">Username</label>
+            <input type="text" value={username} name="username" id="username" />
+          </div>
+          <div>
+            <label htmlFor="email">Username</label>
+            <input type="email" value={email} name="email" id="email" />
+          </div>
+        </form>
+      </FormContainer>
+      <img src={contactUs} alt="" />
     </Container>
   );
 };
@@ -17,4 +31,16 @@ const Container = styled.section`
   padding: 60px 80px;
   background: url(${bg}), no-repeat;
   display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  img {
+    width: 45%;
+  }
+`;
+const FormContainer = styled.div`
+  display: flex;
+  // align-items: center;
+  justify-content: center;
+  flex: 1;
 `;
